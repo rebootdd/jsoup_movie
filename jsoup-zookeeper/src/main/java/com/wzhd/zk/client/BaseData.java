83e0e3ed94806d774edbf33bf60505476f651bf8
xiao_kb <1574821559@qq.com>
2018-02-03 08:37:10 +0800

    monitoring all server program

---
+++ jsoup-zookeeper/src/main/java/com/wzhd/zk/client/BaseData.java
@@ -0,0 +1,10 @@
+package com.wzhd.zk.client;
+
+public class BaseData {
+	
+	protected static final String HOSTS = "127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183";
+	
+	protected static final int SESSION_TIMEOUT = 5000;
+	
+	protected static final String PARENT_PATH = "/jsoup";
+}
